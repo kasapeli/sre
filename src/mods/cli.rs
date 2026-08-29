4f15f70ab5086872a9d30afc0f9abded026eeffe
kasapeli <yanratanaksambath@gmail.com>
2026-08-30 01:16:05 +0700

    add a note on the possibility of deleting categories with remove

--- src/mods/cli.rs
+++ src/mods/cli.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 
 #[derive(Parser)]
 pub struct Args {
-    /// Remove a package. Format: category/package (e.g., editor/vim)
+    /// Remove a package. Format: category/package (e.g., editor/vim). Removing an entire category is also possible.
     #[arg(short = 'r', long = "remove", value_name = "PACKAGE")]
     pub pkgtd: Option<String>,
 
