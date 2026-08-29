f74a9ed6b7b69af5dc5461a202f33dfd4c7ac372
kasapeli <yanratanaksambath@gmail.com>
2026-08-30 01:03:41 +0700

    add short flags

--- src/mods/cli.rs
+++ src/mods/cli.rs
@@ -3,13 +3,13 @@ use clap::Parser;
 #[derive(Parser)]
 pub struct Args {
     /// Remove a package
-    #[arg(long = "remove", value_name = "PACKAGE")]
+    #[arg(short = 'r', long = "remove", value_name = "PACKAGE")]
     pub pkgtd: Option<String>,
 
     /// Specifies the package to install (format: category/package e.g. editor/vim)
-    #[arg(long = "install", value_name = "PACKAGE")]
+    #[arg(short = 'i', long = "install", value_name = "PACKAGE")]
     pub package: Option<String>,
 
-    #[arg(long = "list", value_name = "PATH")]
+    #[arg(short = 'l', long = "list", value_name = "PATH")]
     pub list: Option<String>,
 }
