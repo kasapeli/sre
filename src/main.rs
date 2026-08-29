76656e68283f28b1ebbd76d5641f08dc992c3e19
kasapeli <yanratanaksambath@gmail.com>
2026-08-29 13:31:03 +0700

    add reading from toml

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::{Parser, ValueEnum};
-use std::{env, process::Command};
+use serde::Deserialize;
+use std::{env, fs, io, path::Path, process::Command};
 
 #[derive(ValueEnum, Clone)]
 enum Channels {
@@ -18,6 +19,25 @@ struct Args {
     channel: Channels,
 }
 
+#[derive(Deserialize)]
+struct PackageFile {
+    info: PackageInfo,
+    build: Build,
+}
+
+#[derive(Deserialize)]
+struct PackageInfo {
+    name: String,
+    description: String,
+    version: String,
+    source: String,
+}
+
+#[derive(Deserialize)]
+struct Build {
+    inst: String,
+}
+
 fn main() {
     let args = Args::parse();
 
@@ -28,47 +48,142 @@ fn main() {
 }
 
 fn install_unstable(pkg: &str) {
-    let pkgurl = format!(
+    // Fetching
+    let pkg_fetch_url = format!(
         "https://raw.githubusercontent.com/sre-repo/bin/refs/heads/main/unstable/{pkg}/package.toml"
     );
 
-    println!("Installing {pkg} from {pkgurl}...");
-    println!("Channel: Unstable");
-
-    let home = env::var("HOME").expect("Somehow could not find HOME environment");
-    let path = format!("{}/.srepkgs/{pkg}", home);
-
-    let status = Command::new("wget")
-        .arg("-q")
-        .arg("-nc")
-        .arg("-P")
-        .arg(&path)
-        .arg(&pkgurl)
-        .status()
-        .expect("Exit (Fail)");
-
-    println!("Finish with code {status}");
-}
-
-fn install_stable(pkg: &str) {
-    let pkgurl = format!(
-        "https://raw.githubusercontent.com/sre-repo/bin/refs/heads/main/stable/{pkg}/package.toml"
-    );
-
-    println!("Installing {pkg} from {pkgurl}...");
+    println!("Trying to fetch {pkg} from {pkg_fetch_url}...");
     println!("Channel: Stable");
 
     let home = env::var("HOME").expect("Somehow could not find HOME environment");
-    let path = format!("{}/.srepkgs/{pkg}", home);
+    let path = format!("{home}/.srepkgs/{pkg}");
 
-    let status = Command::new("wget")
+    Command::new("wget")
         .arg("-q")
         .arg("-nc")
         .arg("-P")
         .arg(&path)
-        .arg(&pkgurl)
+        .arg(&pkg_fetch_url)
         .status()
-        .expect("Exit (Fail)");
+        .expect("Failed to download package.toml");
 
-    println!("Finish with code {status}");
-} // TODO: replace wget, actually install packages, handle dependencies, remove packages, handle system-wide packages
+    let read_path = format!("{path}/package.toml");
+
+    match read_package_info(read_path) {
+        Ok(pkginfo) => {
+            println!("Do you want to download the following package?");
+            println!("----------------------------------------------");
+            println!("Name: {}", pkginfo.info.name);
+            println!("Version: {}", pkginfo.info.version);
+            println!("Description: {}", pkginfo.info.description);
+            println!("Source: {}", pkginfo.info.source);
+            println!("[y/n]-----------------------------------------");
+
+            let mut choice = String::new();
+            io::stdin().read_line(&mut choice).unwrap();
+            let choice = choice.trim();
+
+            let build_info = pkginfo.build.inst;
+
+            match choice {
+                "y" | "Y" | "yes" => {
+                    println!("The following command(s) or script(s) will be run:");
+                    println!("---------------------------------------------------");
+                    println!("{}", build_info);
+                    println!("[ENTER]--------------------------------------------");
+
+                    let mut wait = String::new();
+                    io::stdin().read_line(&mut wait).unwrap();
+
+                    let install = Command::new("sh")
+                        .arg("-c")
+                        .arg(&build_info)
+                        .status()
+                        .expect("Failed to install");
+
+                    println!("Finish with code {install}");
+                }
+                "n" | "N" | "no" => {}
+                _ => {}
+            }
+        }
+        Err(e) => {
+            eprintln!("Error reading package.toml: {e}");
+        }
+    }
+}
+
+fn install_stable(pkg: &str) {
+    // Fetching
+    let pkg_fetch_url = format!(
+        "https://raw.githubusercontent.com/sre-repo/bin/refs/heads/main/stable/{pkg}/package.toml"
+    );
+
+    println!("Trying to fetch {pkg} from {pkg_fetch_url}...");
+    println!("Channel: Stable");
+
+    let home = env::var("HOME").expect("Somehow could not find HOME environment");
+    let path = format!("{home}/.srepkgs/{pkg}");
+
+    Command::new("wget")
+        .arg("-q")
+        .arg("-nc")
+        .arg("-P")
+        .arg(&path)
+        .arg(&pkg_fetch_url)
+        .status()
+        .expect("Failed to download package.toml");
+
+    let read_path = format!("{path}/package.toml");
+
+    match read_package_info(read_path) {
+        Ok(pkginfo) => {
+            println!("Do you want to download the following package?");
+            println!("----------------------------------------------");
+            println!("Name: {}", pkginfo.info.name);
+            println!("Version: {}", pkginfo.info.version);
+            println!("Description: {}", pkginfo.info.description);
+            println!("Source: {}", pkginfo.info.source);
+            println!("[y/n]-----------------------------------------");
+
+            let mut choice = String::new();
+            io::stdin().read_line(&mut choice).unwrap();
+            let choice = choice.trim();
+
+            let build_info = pkginfo.build.inst;
+
+            match choice {
+                "y" | "Y" | "yes" => {
+                    println!("The following command(s) or script(s) will be run:");
+                    println!("---------------------------------------------------");
+                    println!("{}", build_info);
+                    println!("[ENTER]--------------------------------------------");
+
+                    let mut wait = String::new();
+                    io::stdin().read_line(&mut wait).unwrap();
+
+                    let install = Command::new("sh")
+                        .arg("-c")
+                        .arg(&build_info)
+                        .status()
+                        .expect("Failed to install");
+
+                    println!("Finish with code {install}");
+                }
+                "n" | "N" | "no" => {}
+                _ => {}
+            }
+        }
+        Err(e) => {
+            eprintln!("Error reading package.toml: {e}");
+        }
+    }
+}
+
+fn read_package_info<P: AsRef<Path>>(path: P) -> Result<PackageFile, Box<dyn std::error::Error>> {
+    let content = fs::read_to_string(path)?;
+    let info: PackageFile = toml::from_str(&content)?;
+
+    Ok(info)
+}
